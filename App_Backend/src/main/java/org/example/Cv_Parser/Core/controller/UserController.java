dc8754f0a8d850f4ae577a4d7968c82dc35d28c0
sami boukesra <sami.boukesra@esprit.tn>
2025-06-23 19:30:46 +0100

    backup

---
+++ App_Backend/src/main/java/org/example/Cv_Parser/Core/controller/UserController.java
@@ -0,0 +1,46 @@
+package org.example.Cv_Parser.Core.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.Cv_Parser.dto.candidate.CreateUserRequest;
+import org.example.Cv_Parser.dto.candidate.UserResponse;
+import org.example.Cv_Parser.Core.services.UserServices;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserServices userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
+        return ResponseEntity.ok(userService.createUser(request));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody CreateUserRequest request) {
+        return ResponseEntity.ok(userService.updateUser(id, request));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+    @GetMapping("/by-email/{email}")
+    public ResponseEntity<UserResponse> getUserByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(userService.getUserByEmail(email));
+    }
+
+
+}
