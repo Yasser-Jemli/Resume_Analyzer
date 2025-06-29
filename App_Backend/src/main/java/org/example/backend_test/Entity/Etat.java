4b27a8e23ccaf34810128a7a5972cf22d53ea432
Yasser-Jemli <yasserjamli37@gmail.com>
2025-06-29 20:07:08 +0100

    update backedn
    Signed-off-by: Yasser-Jemli <yasserjamli37@gmail.com>

--- App_Backend/src/main/java/org/example/backend_test/Entity/Etat.java
+++
@@ -1,8 +0,0 @@
-package org.example.backend_test.Entity;
-
-public enum Etat {
-    InProgress,
-    Opened,
-    Terminated
-}
-
--- App_Backend/src/main/java/org/example/backend_test/Repository/CVDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.backend_test.Repository;
-
-import org.example.backend_test.Entity.CVDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CVDocumentRepository extends JpaRepository<CVDocument, Long> {
-}
